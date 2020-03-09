d245aa9f9fbabea0fd5afd6a7c87a1158e206911
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2020-03-09 14:28:08 +0800

    app settings modification

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-    public defaultAPIURLHost: string = "http://localhost:2650";
+    public defaultAPIURLHost: string = "http://192.168.1.25:8090";
     // public defaultAPIURLHost: string = "http://api.sodex.io";
 }
